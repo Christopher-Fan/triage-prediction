26bd78305f47b1ff98382076565bd3d4083b687e
Christopher Fan <99914744+Christopher-Fan@users.noreply.github.com>
2026-08-14 16:19:38 -0500

    main.py call to predict model based on FastAPI calls

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app = FastAPI(
 
 # --- Request/Response Schema ---
 class PatientVitalRequest(BaseModel):
-    patient_id: str = Field(..., example="CHR-0501")
+    patient_id: str = Field(..., json_schema_extra={"example":"CHR-0501"}, description="Unique patient identifier")
     heart_rate: float = Field(..., ge=30, le= 250, description= "Beats per minute")
     systolic_bp: float = Field(..., ge=50, le=250, description= "Systolic blood pressure (mmHg)")
     oxygen_saturation: float = Field(..., ge=50, le=100, description="Sp02 percentage")
@@ -51,3 +51,49 @@ async def health_check():
         "model_loaded": model_loaded,
         "service": "Triage AI Classifier"
     }
+
+@app.post("/predict", response_model=TriagePredictionResponse)
+async def predict_triage(payload: PatientVitalRequest):
+    model = ml_models.get("triage_classifier")
+    importances = ml_models.get("feature_importances", {})
+
+    if not model:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVICE_ERROR,
+            detail="ML Model has not properly been initialized"
+        )
+
+    # Set the features to match the column order of the model
+    features = np.array([[
+        payload.heart_rate,
+        payload.systolic_bp,
+        payload.oxygen_saturation,
+        payload.temperature_f
+    ]])
+
+    # Execute the prediction of the payload
+    prediction = int(model.predict(features)[0])
+
+    # Rule-based Clinical Factors
+    # Can have added features, but will need to add new feature columns in Model.py
+    risk_factors = []
+
+    if payload.oxygen_saturation < 92: 
+        risk_factors.append("Hypoxia (Sp02 < 92%)")
+    if payload.heart_rate > 100 or payload.heart_rate < 50:
+        risk_factors.append("Abnormal Heart Rate")
+    if payload.systolic_bp < 90 or payload.systolic_bp > 140:
+        risk_factors.append("Abnormal Blood Pressure")
+    if payload.temperature_f > 100.4: 
+        risk_factors.append("Fever")
+
+    priority_map = {0: "ROUTINE", 1: "URGENT", 2: "CRITICAL"}
+
+    return TriagePredictionResponse(
+        patient_id = payload.patient_id,
+        priority_level = priority_map.get(prediction, "UNKNOWN"),
+        risk_score = prediction,
+        risk_factors = risk_factors if risk_factors else ["No acute anomalies detected"],
+        global_feature_importances=importances,
+        status="SUCCESS",
+    )
